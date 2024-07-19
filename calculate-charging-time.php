68fd2b4aac4f2a2076b5c5f937a38aa2d503c5c6
Aditya Kumar <aditya.031298@gmail.com>
2024-07-19 13:03:21 +0530

    stylesheet added

--- calculate-charging-time.php
+++ calculate-charging-time.php
@@ -10,29 +10,35 @@ Author: Aditya Kumar
 function charging_time_form() {
     ob_start();
     ?>
-    <form method="post" action="">
-        <pre>
+    <div class="calc-wrap">
+        <form method="post" action="">
+            <pre>
 Battery (Ah) <input type="number" name="ampMin" step="any" required> 
-        </pre>
-        <input type="submit" name="calculate_time" value="Submit">
-    </form>
-    <?php
-    if (isset($_POST['calculate_time'])) {
-        $ampMin = $_POST['ampMin'];
-        $chargingCurrentPercentage = 10;
-        $chargingLossPercentage = 40;
+Charger (A) <input type="number" name="charAmp" step="any" required> 
+            </pre>
+            <input type="submit" name="calculate_time" value="Submit">
+        </form>
+        <?php
+        if (isset($_POST['calculate_time'])) {
+            $ampMin = $_POST['ampMin'];
+            $charAmp = $_POST['charAmp'];
+            
+            $chargingLossPercentage = 40;
+            $charLoss = $ampMin * ($chargingLossPercentage / 100);
+            $totalAmp = $ampMin + $charLoss;
 
-        $charCurr = $ampMin * ($chargingCurrentPercentage / 100);
-        $charLoss = $ampMin * ($chargingLossPercentage / 100);
-        $totalAmp = $ampMin + $charLoss;
-
-        if ($charCurr != 0) {
-            $timeTaken = $totalAmp / $charCurr;
-            echo "<p>Time Taken = " . $timeTaken . " hours</p>";
-        } else {
-            echo "<p>Error: Charging current cannot be zero.</p>";
+            if ($charAmp != 0) {
+                $timeTaken = $totalAmp / $charAmp;
+                $hours = floor($timeTaken);
+                $minutes = ($timeTaken - $hours) * 60;
+                echo "<p>Time Taken = " . $hours . " hours and " . round($minutes) . " minutes</p>";
+            } else {
+                echo "<p>Error: Charging current cannot be zero.</p>";
+            }
         }
-    }
+        ?>
+    </div>
+    <?php
     return ob_get_clean();
 }
 
@@ -42,3 +48,10 @@ function register_charging_time_shortcode() {
 }
 
 add_action('init', 'register_charging_time_shortcode');
+
+// Enqueue plugin styles
+function enqueue_charging_time_styles() {
+    wp_enqueue_style('charging-time-style', plugin_dir_url(__FILE__) . 'style.css');
+}
+
+add_action('wp_enqueue_scripts', 'enqueue_charging_time_styles');
