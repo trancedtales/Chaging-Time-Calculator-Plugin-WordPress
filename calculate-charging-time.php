c6dbe4d604903d84481a924e3b901b1afe5efd52
Aditya Kumar <aditya.031298@gmail.com>
2024-07-24 11:04:14 +0530

    List update

--- calculate-charging-time.php
+++ calculate-charging-time.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Plugin Name: Calculate Charging Time
-Description: A simple plugin to calculate battery charging time in real time taking losses into account.
+Plugin Name: Calculate Charging Time (Standalone)
+Description: A standalone script to calculate battery charging time.
 Version: 1.0
 Author: Aditya Kumar
 */
@@ -12,17 +12,37 @@ function charging_time_form() {
     ?>
     <div class="calc-wrap">
         <form method="post" action="">
-            <pre>
-Battery (Ah) <input type="number" name="ampMin" step="any" required> 
-Charger (A) <input type="number" name="charAmp" step="any" required> 
-            </pre>
-            <input type="submit" name="calculate_time" value="Submit">
-        </form>
-        <?php
-        if (isset($_POST['calculate_time'])) {
-            $ampMin = $_POST['ampMin'];
-            $charAmp = $_POST['charAmp'];
+            <label for="ampMin">Battery Capacity (Ah):</label>
+            <input type="number" id="ampMin" name="ampMin" step="any" required>
             
+            <label for="charger">Choose a Charger:</label>
+            <select name="charger" id="charger">
+                <option value="5.0">CTEK MXS 5.0 T EU</option>
+                <option value="5">CTEK CT5 Time To Go EU</option>
+                <option value="3.8">CTEK MXS 3.8 EU</option>
+                <option value="7">CTEK MXS 7.0 EU</option>
+                <option value="0.8">CTEK XS 0.8 EU</option>
+                <option value="10">CTEK MXS 10 EU</option>
+                <option value="5">CTEK CT5 Start Stop EU</option>
+                <option value="3.8">CTEK Lithium XS EU</option>
+                <option value="40">CTEK MXTS 40 EU</option>
+                <option value="25">CTEK Pro 25s EU</option>
+                <option value="60">CTEK PRO 60</option>
+                <option value="70">CTEK MXTS 70 - 50</option>
+                <option value="120">CTEK Pro 120</option>
+                <option value="60">CTEK PRO 60 EU</option>
+                <option value="15">CTEK M15</option>
+                <option value="3.8">CTEK CT5 PowerSport</option>
+            </select>
+
+            <input type="submit" name="calculate_time" value="Calculate Time">
+        </form>
+
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['calculate_time'])) {
+            $ampMin = floatval($_POST['ampMin']); // Battery capacity in Ampere-hours
+            $charAmp = floatval($_POST['charger']); // Charging current in Amps
+
             $chargingLossPercentage = 40;
             $charLoss = $ampMin * ($chargingLossPercentage / 100);
             $totalAmp = $ampMin + $charLoss;
